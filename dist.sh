71cbdfd3a1d86547c5772443aa0c29ed32d5896c
Pasi Niemi <pasi.niemi@nitor.com>
2022-01-25 18:03:06 +0200

    Remove old dists in case they exist

--- dist.sh
+++ dist.sh
@@ -3,6 +3,7 @@ rm -rf build dist
 VERSION=$1
 MESSAGE="$2"
 bumpversion --new-version $VERSION --message "$MESSAGE" setup.py
+rm -rf dist
 python setup.py sdist bdist_wheel
 gpg -o dist/cloudformation_utils-${VERSION}-py2.py3-none-any.whl.asc -a -b dist/cloudformation_utils-${VERSION}-py2.py3-none-any.whl
 gpg -o dist/cloudformation-utils-${VERSION}.tar.gz.asc -a -b dist/cloudformation-utils-${VERSION}.tar.gz
